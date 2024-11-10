50338b5b67c312a710d76fcef98b5a110ba4f518
Netanel Rotenstein <email@example.com>
2024-11-10 11:49:33 +0200

    routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
 import express from "express";
 import 'dotenv/config'
+import userController from './controllers/usersController'
+import adminController from './controllers/adminController'
+import votesController from './controllers/votesController'
+import candidatesController from './controllers/candidatesController'
 
 const PORT = process.env.PORT || 3333
 const app = express()
+
+app.use(express.json())
+
+app.use(`/api/users`,userController)
+app.use(`/api/admin`,adminController)
+app.use(`/api/votes`,votesController)
+app.use(`/api/candidates`,candidatesController)
 
 app.listen(PORT, () => { console.log(`server started on port ${PORT}`) })
